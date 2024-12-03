9db531e57796ad0c6c9818d3a7779373e57d2cb6
TU-PJY <viska0319@icloud.com>
2024-12-03 15:19:04 +0900

    안개 활성화

--- Objective-D/ConstantBuffers.cpp
+++ Objective-D/ConstantBuffers.cpp
@@ -45,13 +45,13 @@ void CreateConstantBufferResource(ID3D12Device* Device) {
 
 	// fog data
 	FOG_DATA FogData{
-		{0.5, 0.5, 0.5}, // Fog Color
+		{RGB_(160, 197, 250)}, // Fog Color
 		0.0,   //   padding1
 
-		50.0, // Fog Start
+		600.0, // Fog Start
 		{0.0, 0.0, 0.0}, // padding2
 
-		100.0, // FogEnd
+		700.0, // FogEnd
 		{0.0, 0.0, 0.0} // padding3
 	};
 	ReserveConstantBuffer(FogCBV, 1);
--- Objective-D/Skybox.h
+++ Objective-D/Skybox.h
@@ -15,6 +15,9 @@ public:
 		InitRenderState(RENDER_TYPE_3D);
 		FlipTexture(FLIP_TYPE_NONE);
 		SetLightUse(DISABLE_LIGHT);
+
+		// 스카이박스에서는 안개를 비활성화 한다.
+		SetFogUse(DISABLE_FOG);
 		Transform::Scale(ScaleMatrix, 600.0, 600.0, 600.0);
 		Transform::Move(TranslateMatrix, Position.x, Position.y, Position.z);
 		Render3D(SkyboxMesh, TextureSkybox, 1.0, false);
